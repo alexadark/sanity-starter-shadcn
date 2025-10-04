7b745b36613e6913e0068b784ff522db769ab3df
Alexandra Spalato <alexaspalato@gmail.com>
2025-10-04 10:55:35 +0200

    Refactor section-padding schema: update string quotes for consistency, add initial values for top and bottom padding fields to ensure default behavior in Sanity Studio.

--- sanity/schemas/blocks/shared/section-padding.ts
+++ sanity/schemas/blocks/shared/section-padding.ts
@@ -1,20 +1,22 @@
-import { defineField, defineType } from "sanity";
+import { defineField, defineType } from 'sanity';
 
 export default defineType({
-  name: "section-padding",
-  type: "object",
-  title: "Padding",
-  description: "Add padding to the section. Based on design system spacing",
+  name: 'section-padding',
+  type: 'object',
+  title: 'Padding',
+  description: 'Add padding to the section. Based on design system spacing',
   fields: [
     defineField({
-      name: "top",
-      type: "boolean",
-      title: "Top Padding",
+      name: 'top',
+      type: 'boolean',
+      title: 'Top Padding',
+      initialValue: true,
     }),
     defineField({
-      name: "bottom",
-      type: "boolean",
-      title: "Bottom Padding",
+      name: 'bottom',
+      type: 'boolean',
+      title: 'Bottom Padding',
+      initialValue: true,
     }),
   ],
 });
